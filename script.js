6ebd31981eb6dffbc3279f9da0a806066df8839b
Lucas Enoki <enoki.lucas@gmail.com>
2023-03-11 17:06:02 -0300

    Create 'index.html' and add the basic elements. Create 'script.js' and make and test basic math functions. Add the assignment for 'odin project' to the 'README'

---
+++ script.js
@@ -0,0 +1,26 @@
+//Basic Math Operations
+const add = (a,b) => {
+	const x = a+b;
+	return x;
+}
+
+const subtract = (a,b) => {
+	const x = a-b;
+	return x;
+}
+
+const multiply = (a,b) => {
+	const x = a*b;
+	return x;
+}
+
+const divide = (a,b) => {
+	const x = a/b;
+	return x;
+}
+
+//Testing the previous functions.
+console.log(add(3,2));
+console.log(subtract(3,20));
+console.log(multiply(3,2));
+console.log(divide(3,2));
