29f1095f39e57ef789fa265150ac211e88d9b20f
Lucas Enoki <enoki.lucas@gmail.com>
2023-03-11 17:10:47 -0300

    Create 'operate(operation,a,b)'. calls one function and run it using the 2 numbers.

--- script.js
+++ script.js
@@ -19,8 +19,17 @@ const divide = (a,b) => {
 	return x;
 }
 
+/*
 //Testing the previous functions.
 console.log(add(3,2));
 console.log(subtract(3,20));
 console.log(multiply(3,2));
 console.log(divide(3,2));
+*/
+
+
+const operate = (operation, a, b) => {
+	return operation(a,b);
+}
+
+console.log(operate(add, 2, 3));
